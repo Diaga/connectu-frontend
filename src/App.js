cd5b170698b45fda631d04a89c670fb7574e7b8f
Arwah Muhammad Jawwad <arwah.jawwad.aj@gmail.com>
2020-12-08 19:20:07 +0500

    university popup added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import {HashRouter, Route} from 'react-router-dom';
 import Login from './containers/Login/login';
-import {SignupGroup} from './containers/Signup/signupGroup';
+import Signup from './containers/Signup/signup';
+import Degree from './containers/Signup/degree';
+import Interests from './containers/Signup/interests';
 import Home from './containers/Home/home';
 import Mentors from './containers/Pairing/mentor';
 import Professional from './containers/Pairing/professional';
+
 
 
 
@@ -12,14 +15,13 @@ import './App.css';
 function App() {
     return (
         <HashRouter>
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/login" component={Login}/>
-            <Route exact path="/signup" component={SignupGroup}/>
-            <Route exact path="/mentors" component={Mentors}/>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/mentors" component={Mentors} />
-            <Route exact path="/professionals" component={Professional} />
+            <Route exact path="/" component={Home}></Route>
+            <Route exact path="/login" component={Login}></Route>
+            <Route exact path="/signup" component={Signup}></Route>
+            <Route exact path="/degree" component={Degree}></Route>
+            <Route exact path="/interests" component={Interests}></Route>
+            <Route exact path="/mentors" component={Mentors}></Route>
+            <Route exact path="/professionals" component={Professional}></Route>
         </HashRouter>
 
     );
--- src/containers/Signup/signup.js
+++ src/containers/Signup/signup.js
@@ -1,119 +1,111 @@
-import React, {Component} from "react";
-import {
-    Container,
-    Box,
-    Typography,
-    TextField,
-    Radio,
-    RadioGroup,
-    FormControlLabel,
-    IconButton
-} from "@material-ui/core";
-import {withStyles} from "@material-ui/core";
-import {blueGrey} from "@material-ui/core/colors";
+import React from "react";
+import { Container, Box, Typography, TextField, Radio, RadioGroup, FormControlLabel, IconButton } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import { blueGrey } from "@material-ui/core/colors";
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 
 
-const useStyles = (theme) => ({
-    heading: {
+const useStyles = makeStyles((theme) => ({
+    heading:{
         fontWeight: 700,
         fontSize: 36
     },
-    subheading: {
+    subheading:{
         fontWeight: 500
     },
-    button: {
+    button:{
         width: '40%'
     },
-    text: {
+    text:{
         fontWeight: 700
     }
 
 
-});
+}))
 
 
-export class Signup extends Component {
 
-    render() {
-        const classes = this.props;
+const Signup = () => {
+    const classes= useStyles();
+    const [value, setValue] = React.useState('');
 
-        return (
-            <Container maxWidth="xs">
-                <Box
-                    textAlign="center"
-                    p="24px"
-                    mt="50px"
-                    bgcolor="white"
-                    boxShadow="3"
-                >
+    const handleChange = (event) => {
 
-                    <Typography variant="h3" color="primary" className={classes.heading}>
-                        Signup
-                    </Typography>
-                    <br/>
-                    <Typography variant="body1" color="secondary" className={classes.subheading}>
-                        Signup your account
-                    </Typography>
-                    <br/>
-
-                    <TextField
-                        id="outlined-size-small"
-                        label="Username"
-                        variant="outlined"
-                        size="small"
-                        fullWidth
-                        margin="normal"
-
-                        onChange={(event) => this.props.handlePage({username: event.target.value})}
-                    />
-                    <TextField
-                        id="outlined-size-small"
-                        label="Email"
-                        variant="outlined"
-                        size="small"
-                        fullWidth
-                        margin="normal"
-
-                        onChange={(event) => this.props.handlePage({email: event.target.value})}
-                    />
-                    <TextField
-
-                        id="outlined-size-small"
-                        label="Password"
-                        variant="outlined"
-                        size="small"
-                        fullWidth
-                        margin="normal"
-
-                        onChange={(event) => this.props.handlePage({password: event.target.value})}
-                    />
-                    <Typography variant="body2" color="primary"
-                                style={{textAlign: 'left', fontSize: 16, fontWeight: 550}}>You wish to sign up
-                        as:</Typography>
-                    <RadioGroup aria-label="role" name="role" row value={this.props.role}
-                                onChange={(event, value) => this.props.handlePage({role: event.target.value})}>
-                        <FormControlLabel value="student" control={<Radio/>} label="Student"/>
-                        <FormControlLabel value="mentor" control={<Radio/>} label="Mentor"/>
-                    </RadioGroup>
+      setValue(event.target.value);
 
 
-                    <IconButton style={{float: 'right'}} onClick={() => this.props.handlePage({page: 'degrees'})}>
-                        <ArrowForwardIcon/>
-                    </IconButton>
-                    <br/>
+    };
+  return (
+    <Container maxWidth="xs">
+      <Box
+        textAlign="center"
+        p="24px"
+        mt="50px"
+        bgcolor="white"
+        boxShadow="3"
+      >
+
+        <Typography variant="h3" color="primary" className={classes.heading}>
+          Signup
+        </Typography>
+        <br/>
+        <Typography variant="body1" color="secondary" className={classes.subheading}>
+          Signup your account
+        </Typography>
+        <br/>
+
+        <TextField
+          id="outlined-size-small"
+          label="Username"
+          variant="outlined"
+          size="small"
+          fullWidth
+          margin="normal"
+
+        />
+        <TextField
+          id="outlined-size-small"
+          label="Email"
+          variant="outlined"
+          size="small"
+          fullWidth
+          margin="normal"
+
+        />
+        <TextField
+
+          id="outlined-size-small"
+          label="Password"
+          variant="outlined"
+          size="small"
+          fullWidth
+          margin="normal"
+        />
+        <Typography variant="body2" color="primary" style={{textAlign: 'left', fontSize: 16, fontWeight: 550}}>You wish to sign up as:</Typography>
+        <RadioGroup aria-label="role" name="role" row value={value} onChange={handleChange}>
+    <FormControlLabel value="student" control={<Radio />} label="Student" />
+    <FormControlLabel value="mentor" control={<Radio />} label="Mentor" />
+    </RadioGroup>
 
 
-                    <Typography variant="body2" color="secondary" className={classes.text}>
-                        Already have an account? <span style={{color: blueGrey[500]}}><a
-                        href="/#/login">Login</a></span>
-                    </Typography>
+    <a href="/#/degree">
+    <IconButton style={{float: 'right'}}>
+        <ArrowForwardIcon/>
+    </IconButton>
+
+    </a>
+
+    <br/>
 
 
-                </Box>
-            </Container>
-        );
-    }
+        <Typography variant="body2" color="secondary" className={classes.text}>
+          Already have an account? <span style={{color: blueGrey[500]}}><a href="/#/login">Login</a></span>
+        </Typography>
+
+
+      </Box>
+    </Container>
+  );
 };
 
-export default withStyles(useStyles, {withTheme: true})(Signup);
+export default Signup;
